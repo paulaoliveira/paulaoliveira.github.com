18945188dab55d4417501bb28a67c30fa7ba3df8
Nuno Job <nunojobpinto@gmail.com>
2011-09-23 16:26:43 +0200

    blergh

--- static/js/routes.js
+++ static/js/routes.js
@@ -1,11 +1,12 @@
 ko.kickstart_ss = function (v) {
   var routes          =
       { '/home'           : { on: function() {} }
-      , '/people'         : { on: ko.visuals.show_staff  }
+      , '/people'         : { on: ko.visuals.list_staff  }
       , '/people/:person' : { on: ko.visuals.show_person(v.selected_person_name) }
       , '/research'       : { on: function() {} }
       , '/publications'   : { on: function() {} }
-      , '/news'           : { on: function() {} }
+      , '/news'           : { on: ko.visuals.list_news }
+      , '/news/:perma'    : { on: ko.visuals.show_news(v.selected_news_permalink) }
       , '/contact'        : { on: function() {} }
       }
     , router          = Router(routes)
--- static/visuals.js
+++ static/visuals.js
@@ -1,26 +1,39 @@
 $(document).ready(function() {  
   ko.visuals = {};
-  ko.visuals.show_person = function (person_name) {
+  ko.visuals.show_person = function (name_observable) {
       return function show_person(name) {
         name = unescape(name);
-        $("#list_staff").hide();
-        if(person_name  && person_name()  !== name)  { person_name(name); }
-        $("#person").show();
+        $("#people_list").hide();
+        if(name_observable  && name_observable()  !== name)  { name_observable(name); }
+        $("#people_show").show();
       };
     };
 
-  ko.visuals.show_staff = 
-    function show_staff() {
-      $("#list_staff").show();
-      $("#person").hide();
+  ko.visuals.list_staff = 
+    function list_staff() {
+      $("#people_list").show();
+      $("#people_show").hide();
     };
-  
+
+  ko.visuals.show_news = function (permalink_observable) {
+      return function show_news(permalink) {
+        $("#news_list").hide();
+        if(permalink_observable  && permalink_observable()  !== permalink)  { permalink_observable(permalink); }
+        $("#news_show").show();
+      };
+    };
+
+  ko.visuals.list_news = 
+    function list_snews() {
+      $("#news_list").show();
+      $("#news_show").hide();
+    };
+
   ko.visuals.show_sections = function (router) {
       return function show_sections() {
         var current_section = router.getRoute(0)
-          , main_sections   = $('section[data-route-0="' + current_section + '"]')
+          , main_sections   = $('section[data-route="' + current_section + '"]')
           , nav_current     = $('#nav #nav_' + current_section)
-          , nav_current_a   = $('#nav #nav_' + current_section)
           ;
         $('section').hide();
         main_sections.show();
