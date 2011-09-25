adce702ee467d4217005a97c71d26ef902c769bc
Nuno Job <nunojobpinto@gmail.com>
2011-09-25 08:29:37 -0700

    [static] static refactoring

--- server/cfg/environment.js
+++ server/cfg/environment.js
@@ -2,14 +2,12 @@ var express = require('express')
   , path    = require('path')
   ;
 
-console.log(path.normalize(__dirname + '/../../static'))
-
 module.exports =
   function (app) {
-    app.configure(function(){
+    app.configure(function() {
       app.use(express.bodyParser());
       app.use(app.router);
-      app.use(express.static('/../../static'));
+      app.use(express.static(__dirname + '/../..'));
     });
     process.on('uncaughtException', function(err) {
       console.log(JSON.stringify(err));
--- static/helpers.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  ko.app.helpers = {};
-  ko.app.helpers.except_last = function(current,collection) {
-    return !(current === collection[collection.length-1]);
-  };
-});
